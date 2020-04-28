9c2b9d65fcfb71c5e9b4baf907a6332b38659ad6
Clint Hart <clinthart@comcast.net>
2020-04-28 13:35:51 -0400

    Added docstring comments
    Added docstring comments

--- TRowTest.py
+++ TRowTest.py
@@ -2,6 +2,14 @@ import string
 import re
 
 def long_word_counter(sentance='The cow jumped over the moon.'):
+    """
+    This method looks for the longest word in a sentance. The return
+    is a list containing the longest word and its length as an int.
+    Special characters are removed from being counted.
+
+    Example:  long_word_counter(sentance='The cow jumped over the moon.')
+
+    """
     sentance = re.sub(r"[,.;@#?!&$]+", '', sentance).split()  #remove all punctuations from sentances
     count = 0
     lword = ''
@@ -16,6 +24,15 @@ def long_word_counter(sentance='The cow jumped over the moon.'):
 
 
 def short_word_counter(sentance = 'The cow jumped over the moon.'):
+    """
+        This method looks for the shortest word in a sentance. The return
+        is a list containing the shortest word and its length as an int.  The
+        words "the" and "a" are intentionally ignored.  Special characters are
+        removed from being counted.
+
+        Example:  short_word_counter(sentance='The cow jumped over the moon.')
+
+        """
     sentance = re.sub(r"[,.;@#?!&$]+", '', sentance).split()   #remove all punctuations from sentances
     count = 0
     lword = ''
