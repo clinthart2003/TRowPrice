b73967ff8cabac77148cab14715edd6ade0958bb
Clint Hart <clinthart@comcast.net>
2020-04-28 14:51:40 -0400

    fix logic in short test being missing
    fix logic in short test being missing

--- Unit_Test_2.py
+++ Unit_Test_2.py
@@ -1,9 +1,13 @@
 from TRowTest import *
 
-test_sentances = [['The cow jumped over the moon.', 'jumped', 6],
-                  ['The cow jumped over the moon?', 'jumped', 6],
-                  ['Add a method that returns the shortest word and length with unit tests!', 'Add', 9],
-                  ['Add a method that returns the shortest word and length with unit tests!',, 'shortest', 8]]
+test_sentances_long = [['The cow jumped over the moon.', 'jumped', 6],
+                       ['The cow jumped over the moon?', 'jumped', 6]
+                       ]
+
+
+test_sentances_short = [['Add a method that returns the shortest word and length with unit tests!', 'Sentance', 8],
+                        ['Add a method that returns the shortest word and length with unit tests!', 'Add', 3]
+                        ]
 
 def unittest():
     """
@@ -15,18 +19,26 @@ def unittest():
     test Passed: cow, 3
     Test Failed: Add, 3   Intentionally FAILED test
     test Passed: shortest, 8
-    
     """
-    for test in test_sentances:
+    for test in test_sentances_long:
         try:
             results = (long_word_counter(test[0]))
-            if (results[0] == test[2]) and (results[1]==test[3]):
+            if (results[0] == test[1]) and (results[1]==test[2]):
                 print ("test Passed: " + results[0] + ", " + str(results[1]))
             else:
-                print ("test Failed: " + results[0] + ", " + str(results[1]))
+                print ("test Failed: " + results[0] + ", " + str(results[1]) + " expected: " + test[1] +", " + str(test[2]))
         except Exception as e:
             print (e)
 
+    for test in test_sentances_short:
+        try:
+            results = (short_word_counter(test[0]))
+            if (results[0] == test[1]) and (results[1]==test[2]):
+                print ("test Passed: " + results[0] + ", " + str(results[1]))
+            else:
+                print("test Failed: " + results[0] + ", " + str(results[1]) + " expected: " + test[1] + ", " + str(test[2]))
+        except Exception as e:
+            print (e)
 
 if __name__ == '__main__':
     unittest()
