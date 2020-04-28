f9066ba21b73e1ac646c2ea308243fb127a4091f
Clint Hart <clinthart@comcast.net>
2020-04-28 14:02:00 -0400

    quick and dirty try's added
    quick and dirty try's added.   This ensures all tests run in case there is an exception that occurs.

--- Unit_Test.py
+++ Unit_Test.py
@@ -2,19 +2,23 @@ from TRowTest import *
 
 def unittest():
 
-    results = (long_word_counter(sentance = 'The cow jumped over the moon.'))
-    if (results[0] == 'jumped') and (results[1]==6):
-        print ("test Passed: " + results[0] + ", " + str(results[1]))
-    else:
-        print ("test Failed: " + results[0] + ", " + str(results[1]))
+    try:
+        results = (long_word_counter(sentance = 'The cow jumped over the moon.'))
+        if (results[0] == 'jumped') and (results[1]==6):
+            print ("test Passed: " + results[0] + ", " + str(results[1]))
+        else:
+            print ("test Failed: " + results[0] + ", " + str(results[1]))
+    except Exception as e:
+        print (e)
 
-
-    results = (short_word_counter(sentance='The cow jumped over the moon?'))
-    if results[0] == 'cow' and results[1]==3:
-        print ("test Passed: " + results[0] + ", " + str(results[1]))
-    else:
-        print ("Test Failed: " + results[0] + ", " + str(results[1]))
-
+    try:
+        results = (short_word_counter(sentance='The cow jumped over the moon?'))
+        if results[0] == 'cow' and results[1]==3:
+            print ("test Passed: " + results[0] + ", " + str(results[1]))
+        else:
+            print ("Test Failed: " + results[0] + ", " + str(results[1]))
+    except Exception as e:
+        print (e)
 
     results = (short_word_counter(sentance='Add a method that returns the shortest word and length with unit tests!'))
     if (results[0] == 'Add') and (results[1]==9):
@@ -22,12 +26,14 @@ def unittest():
     else:
         print ("Test Failed: " + results[0] + ", " + str(results[1]) + "   Intentionally FAILED test")
 
-
-    results = (long_word_counter(sentance='Add a method that returns the shortest word and length with unit tests!'))
-    if results[0] == 'shortest' and results[1]==8:
-        print ("test Passed: " + results[0] + ", " + str(results[1]))
-    else:
-        print ("Test Failed: " + results[0] + ", " + str(results[1]))
+    try:
+        results = (long_word_counter(sentance='Add a method that returns the shortest word and length with unit tests!'))
+        if results[0] == 'shortest' and results[1]==8:
+            print ("test Passed: " + results[0] + ", " + str(results[1]))
+        else:
+            print ("Test Failed: " + results[0] + ", " + str(results[1]))
+    except Exception as e:
+        print (e)
 
 if __name__ == '__main__':
     unittest()
